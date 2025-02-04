c34201b1e111c8bd8a539f74775f3ef32351cbd7
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-04 12:57:41 -0500

    Update Home component to display CampaignDetail instead of TodoList and change header title.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import TodoList from "./_components/TodoList";
+import CampaignDetail from "./_components/CampaignDetail";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">tRPC + Next.js 14 App Router</h1>
-      <TodoList />
+      <h1 className="text-4xl font-bold">Banzai Demo APP</h1>
+      <CampaignDetail campaignId="123" />
     </div>
   );
 }
