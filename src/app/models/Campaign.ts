f52bb3d52272b3f7613e80ba7c25845a1b9090d8
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-03 23:36:50 -0500

    Add timestamps to Campaign schema.

--- src/app/models/Campaign.ts
+++ src/app/models/Campaign.ts
@@ -1,11 +1,15 @@
+import { time } from "console";
 import mongoose from "mongoose";
 import { string } from "zod";
 
-const CanmpaignSchema = new mongoose.Schema({
-  name: string,
-  spend: Number,
-  clicks: Number,
-  conversions: Number,
-});
+const CanmpaignSchema = new mongoose.Schema(
+  {
+    name: string,
+    spend: Number,
+    clicks: Number,
+    conversions: Number,
+  },
+  { timestamps: true }
+);
 export default mongoose.models.Campaign ||
   mongoose.model("Campaign", CanmpaignSchema);
