c2720d339ed273e59857ba3d4aa992da9c758b7c
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-04 12:51:31 -0500

    Remove commented-out appRouter implementation from trpc.ts.

--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -9,24 +9,3 @@ const t = initTRPC.create();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
-
-// export const appRouter = t.router({
-//   getCampaigns: t.procedure.query(async () => {
-//     return await Campaign.find();
-//   }),
-
-//   addCampaign: t.procedure
-//     .input(
-//       z.object({
-//         name: z.string(),
-//         spend: z.number(),
-//         clicks: z.number(),
-//         conversions: z.number(),
-//       })
-//     )
-//     .mutation(async ({ input }) => {
-//       return await Campaign.create(input);
-//     }),
-// });
-
-// export type AppRouter = typeof appRouter;
