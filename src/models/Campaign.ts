1ac1b025802220f3cb00a060052046736aa6c133
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-04 08:54:55 -0500

    Add Campaign model schema for managing campaign data.

---
+++ src/models/Campaign.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { string } from "zod";
+
+const CanmpaignSchema = new mongoose.Schema(
+  {
+    name: string,
+    spend: Number,
+    clicks: Number,
+    conversions: Number,
+  },
+  { timestamps: true }
+);
+export default mongoose.models.Campaign ||
+  mongoose.model("Campaign", CanmpaignSchema);
