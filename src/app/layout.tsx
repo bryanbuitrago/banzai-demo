4fc330fe8ba1fc7be2d8e60b69dd5ac4fe18f44f
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-04 08:56:08 -0500

    Replace TrpcProvider with Provider component in RootLayout for improved tRPC integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
-import { TrpcProvider } from "./utils/trpc-provider";
-
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import Provider from "@/app/_trpc/Provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TrpcProvider>{children}</TrpcProvider>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
