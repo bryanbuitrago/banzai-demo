a8e0c7ee38172400d9f012c339b084d0819ec931
Bryan Buitrago <bryanbuitrago86@gmail.com>
2025-02-04 12:55:44 -0500

    Refactor Campaign model to use campaignId as _id and set default values for spend and conversions.

--- src/models/Campaign.ts
+++ src/models/Campaign.ts
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
-import { string } from "zod";
 
-const CanmpaignSchema = new mongoose.Schema(
-  {
-    name: string,
-    spend: Number,
-    clicks: Number,
-    conversions: Number,
-  },
-  { timestamps: true }
-);
-export default mongoose.models.Campaign ||
-  mongoose.model("Campaign", CanmpaignSchema);
+const CampaignSchema = new mongoose.Schema({
+  _id: String, // Use campaignId as _id
+  spend: { type: Number, default: 0 },
+  conversions: { type: Number, default: 0 },
+});
+
+const Campaign =
+  mongoose.models.Campaign || mongoose.model("Campaign", CampaignSchema);
+export default Campaign;
